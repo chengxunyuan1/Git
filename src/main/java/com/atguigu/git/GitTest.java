c5c3425e8b6bfb99a3cc2dbfba39d6175c6b72ab
zhb <1938290727@qq.com>
2022-04-01 14:37:13 +0800

    push commit11

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("push push");
         System.out.println("pull pull");
-        
+        System.out.println("push");
+
     }
 }
